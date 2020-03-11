e734ee89d274dd2c92ccd62dc248b8811a1fbaa8
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-03-11 18:38:43 +0100

    Set translator demo for production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // posApiUrl: 'http://temu.bsc.es:80/api/pos',
-  // translatorApiUrl: 'http://temu.bsc.es:80/api/translator',
-  posApiUrl: 'http://localhost:5000',
-  // translatorApiUrl: 'http://localhost:5001',
-  translatorApiUrl: 'http://localhost:80/api',
+  // posApiUrl: 'http://temu.bsc.es:8080/api/pos',
+  // translatorApiUrl: 'http://temu.bsc.es:8080/api/translator',
+  posApiUrl: 'http://localhost:8080/api/pos',
+  translatorApiUrl: 'http://localhost:8080/api/translator',
 }
