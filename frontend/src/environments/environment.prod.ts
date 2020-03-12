51e00b8c6e67da2018449c961ddc3511edaea4b8
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-03-12 11:27:04 +0100

    Fix production environment endpoint variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // posApiUrl: 'http://temu.bsc.es:8080/api/pos',
-  // translatorApiUrl: 'http://temu.bsc.es:8080/api/translator',
-  posApiUrl: 'http://localhost:8080/api/pos',
-  translatorApiUrl: 'http://localhost:8080/api/translator',
+  posApiUrl: 'http://temu.bsc.es:8080/api/pos',
+  translatorApiUrl: 'http://temu.bsc.es:8080/api/translator',
+  // posApiUrl: 'http://localhost:8080/api/pos',
+  // translatorApiUrl: 'http://localhost:8080/api/translator',
 }
