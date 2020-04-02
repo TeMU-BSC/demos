65ae489d2edc1e7541861693d54e3f995ebd03f4
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-04-02 16:03:48 +0200

    npm update and change environment prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,17 +1,5 @@
-// export const environment = {
-//   production: true,
-//   posApiUrl: 'http://temu.bsc.es:8080/api/pos',
-//   translatorApiUrl: 'http://temu.bsc.es:8080/api/translator',
-// }
-
-// export const environment = {
-//   production: true,
-//   posApiUrl: 'http://localhost:8080/api/pos',
-//   translatorApiUrl: 'http://localhost:8080/api/translator',
-// }
-
 export const environment = {
   production: true,
-  posApiUrl: 'http://84.88.51.174:8080/api/pos',
-  translatorApiUrl: 'http://84.88.51.174:8080/api/translator',
+  posApiUrl: 'http://temu.bsc.es:8080/api/pos',
+  translatorApiUrl: 'http://temu.bsc.es:8080/api/translator',
 }
